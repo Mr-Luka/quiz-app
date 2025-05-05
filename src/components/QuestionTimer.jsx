957a7a43611302d90fce33975b0fbe6f6b440bac
Luka <luka.luka.luka93@gmail.com>
2025-05-05 12:34:06 -0700

    cleaned the code starting over quizz component

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,32 +1,13 @@
 import {useState, useEffect} from 'react';
 
 export default function QuestionTimer({timeout, onTimeout}){
-    const [remainingTime, setRemainingTime] = useState(timeout);
 
-    useEffect(()=> {
-        console.log('SETTING TIMEOUT')
-        const timer = setTimeout(onTimeout, timeout);
-
-        return () => {
-            clearTimeout(timer); // clear time
-        }
-    }, [timeout, onTimeout])
     // because we aer using two props onTimeout and timeout, we need to populate dependecies
     // setTimeout(()=> {
     //     onTimeout();
     // }, timeout)
 
-    useEffect(()=> {
-        console.log('SETTING INTERVAL')
-        const interval = setInterval(()=> {
-        setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
-    }, 100);
+ 
 
-        return () => {
-            clearInterval(interval);
-        }
 
-    }, []);
-
-    return <progress id='question-time' max={timeout} value={remainingTime}/>
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,42 +7,28 @@ export default function Quizz(){
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
-    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
+    function handleSelectAnswer(selectedAnswer){
         setUserAnswers(prevUserAnswers=> {
-            return [...prevUserAnswers, selectedAnswer];
-        });
-    }, []);
-
-    const handleSkipAnswer = useCallback(()=> handleSelectAnswer(null), [handleSelectAnswer]);
-
-    if(quizIsComplete) {
-        return <div id='summary'>
-        <img src={quizCompleteImage} alt='Trophy icon' />
-            <h2>Quiz Completed</h2>
-        </div>
+            console.log("New array:", [...prevUserAnswers, selectedAnswer])
+            return [...prevUserAnswers, selectedAnswer]
+        })
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-    shuffledAnswers.sort(()=> Math.random() - 0.5) // will not return new array, buy edit the array
 
     return (
-        <div id="quiz">
-            <div id="question">
-                <QuestionTimer 
-                    key={activeQuestionIndex}
-                    timeout={10000} 
-                    onTimeout={()=> handleSelectAnswer(null)} 
-                />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map(answer => <li key={answer} className='answer'>
-                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                    </li>)}
-                </ul>
-            </div>
+       <div id='quiz'>
+         <div id='question'>
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id='answers'>
+                {QUESTIONS[activeQuestionIndex].answers.map(answer=>  (
+                    <li key={answer} className='answer'>
+                        <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+            </ul>
         </div>
+       </div>
     )
 }
 

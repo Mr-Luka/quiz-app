c0df490d48789f0fb0929db1da196a4aa5bb88a9
Luka <luka.luka.luka93@gmail.com>
2025-05-08 13:35:45 -0700

    added personalized name

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
     return (
         <>
-        <Destinations />
+        <HomePage />
             {/* {userRegistered 
             ? 
                 <HomePage /> 
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,31 +1,47 @@
 import AroundTheWorldHomePage from '../../../public/home-page.mp4';
+import Destinations from './Destinations/Destinations.jsx';
 
 import './HomePage.css';
 
+import {useState} from 'react';
+
 export default function HomePage(){
+    const [showDestinations, setShowDestination] = useState(false);
+
+    function handleNavigate(destination) {
+        if(destination === 'Destinations'){
+            setShowDestination(true);
+        } else {
+            setShowDestination(false);
+        }
+    }
 
 
     return (
-        <>
-            <div className="wrapper-home">
-            <div className="video-home">
-                <video id="home-video" src={AroundTheWorldHomePage} autoPlay loop muted></video>
-            </div>
-            <div className="header">
-                <div className='welcome-universe-tour'>
-                    <h3>Name<br/> Welcome to the</h3>
-                    <h1>Universe Tour</h1>
-                </div>
-                <div className="nav-bar">
-                    <ul className="nav-ul">
-                        <li>Home</li>
-                        <li>Destinations</li>
-                        <li>Crew</li>
-                        <li>Technology</li>
-                    </ul>
-                </div>
-            </div>
-            </div>
-        </>
-    )
-}
+    <div className="wrapper-home">
+      {!showDestinations && (
+        <div className="video-home">
+          <video id="home-video" src={AroundTheWorldHomePage} autoPlay loop muted></video>
+        </div>
+      )}
+      <div className="header">
+        <div className="welcome-universe-tour">
+          <h1>Universe TourX</h1>
+        </div>
+        <div className="nav-bar">
+          <ul className="nav-ul">
+            <li onClick={() => handleNavigate('Home')}>Home</li>
+            <li onClick={() => handleNavigate('Destinations')}>Destinations</li>
+            <li onClick={() => handleNavigate('Crew')}>Crew</li>
+            <li onClick={() => handleNavigate('Technology')}>Technology</li>
+          </ul>
+        </div>
+      </div>
+      {!showDestinations && (
+        <div className='welcome-name'>
+            <h2>First LastName<br/> <span>Welcome</span><br/> to YOUR journey</h2>
+        </div>
+      )}
+      {showDestinations && <Destinations />}
+    </div>
+)}

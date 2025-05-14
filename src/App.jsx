efa08ab6a9ebf1ca39c165fa1b5a0d6af7f95d40
Luka <luka.luka.luka93@gmail.com>
2025-05-14 12:48:28 -0700

    fixed the css issue for the scroll on welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,24 @@
 import './index.css';
-import {useState} from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-import WelcomePage from './components/welcome-page.jsx';
-import HomePage from './components/HomePage/HomePage.jsx';
-import HomePageLayout from './components/HomePage/Home-page-layout';
-import Crew from './components/HomePage/Crew/Crew.jsx';
-
-import Exodus from './components/HomePage/Destinations/Exodus/Exodus.jsx';
-import Plantia from './components/HomePage/Destinations/Plantia/Plantia.jsx';
-import Mars from './components/HomePage/Destinations/Mars/Mars.jsx';
+import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './AppRouts.jsx';
 
 function App() {
-  const [userRegistered, setUserRegistered] = useState(false); 
+  const [userRegistered, setUserRegistered] = useState(false);
   const [userName, setUserName] = useState('Explorer');
 
-  // Function to handle successful registration
   const handleRegistrationSuccess = (name) => {
-    setUserName(name)
+    setUserName(name);
     setUserRegistered(true);
   };
 
-   return (
+  return (
     <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            userRegistered ? (
-              <Navigate to="/home" replace />
-            ) : (
-              <WelcomePage onRegisterSuccess={handleRegistrationSuccess} />
-            )
-          }
-        />
-        <Route path="/home" element={<HomePage userName={userName} />} />
-        <Route path="/destinations" element={<HomePage showDestinations={true} userName={userName} />} />
-        <Route path="/exodus" element={<HomePageLayout><Exodus /></HomePageLayout>} />
-        <Route path="/plantia" element={<HomePageLayout><Plantia /></HomePageLayout>} />
-        <Route path="/mars" element={<HomePageLayout><Mars /></HomePageLayout>} />
-        <Route path="/crew" element={<HomePageLayout><Crew /></HomePageLayout>} />
-      </Routes>
+      <AppRoutes
+        userRegistered={userRegistered}
+        userName={userName}
+        handleRegistrationSuccess={handleRegistrationSuccess}
+      />
     </Router>
   );
 }
